3bf58d7bc3704411c7b0a5b32a79d2e11b353f94
paknar <prahitajanar@gmail.com>
2017-11-13 17:49:47 +0700

    adding multithread

--- client_socket.py
+++ client_socket.py
@@ -1,7 +1,8 @@
 import socket
 import sys
-
-port = 12346
+import threading
+from funcrecv import Receive, Sending
+port = 12345
 host = 'localhost'
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -12,15 +13,9 @@ except socket.error as msg:	#if we got error,close the socket and exit
 	print("Cannot connect to server")
 	sys.exit(1)
 if sock is not None:
-	while True:
-		data = input()
-		#print('a', data.lstrip(), 'a', '\r', 'a')
-		if data.lstrip() != '' :
-			sock.send(data.encode())	#encode the user message so it can be send with sock.send()
-			incoming = sock.recv(2048).decode()	#decode incoming message from server
-			if incoming == "Connection closed":	#if server close connection,close socket on client too
-				print("Exit Code Detected, Connection Closed")
-				sock.close()
-				break
-			print(incoming)	#print server message
-	sys.exit(1)
+	recv=Receive(sock)
+	send=Sending(sock)
+	recv.start()
+	send.start()
+	
+sys.exit(1)
